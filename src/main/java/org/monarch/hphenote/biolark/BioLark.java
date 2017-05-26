40598e2d65025e2851f74db51d49dcb18a461249
pnrobinson <peter.robinson@jax.org>
2017-05-26 15:11:02 -0400

    Starting to add REST/JSON code

---
+++ src/main/java/org/monarch/hphenote/biolark/BioLark.java
@@ -0,0 +1,12 @@
+package org.monarch.hphenote.biolark;
+
+/**
+ * This class is responsible for orgnazing the results of Bio-Lark based parsing of HPO texts.
+ * See: Groza T,et al. (2015) Automatic concept recognition using the human
+ * phenotype ontology reference and test suite corpora. Database (Oxford). pii: bav005.
+ * This class will store results of parsing and present an interface for the JavaFX wdiget that
+ * will present the parsed text and suggested HPO terms for vetting by the user.
+ * Created by robinp on 5/26/17.
+ */
+public class BioLark {
+}
---
+++ src/test/java/org/monarch/hphenote/biolark/BioLarkTest.java
@@ -0,0 +1,7 @@
+package org.monarch.hphenote.biolark;
+
+/**
+ * Created by robinp on 5/26/17.
+ */
+public class BioLarkTest {
+}
