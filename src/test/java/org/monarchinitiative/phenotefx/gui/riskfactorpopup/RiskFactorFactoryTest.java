6cab34ac398f1f2864d0c5a8fd688a699ed4e444
Aaron Zhang <kingmanzhang@gmail.com>
2019-06-21 14:28:05 -0400

    update

--- src/test/java/org/monarchinitiative/phenotefx/gui/riskfactorpopup/RiskFactorFactoryTest.java
+++ src/test/java/org/monarchinitiative/phenotefx/gui/riskfactorpopup/RiskFactorFactoryTest.java
@@ -1,11 +1,32 @@
 package org.monarchinitiative.phenotefx.gui.riskfactorpopup;
 
+/*
+ * #%L
+ * PhenoteFX
+ * %%
+ * Copyright (C) 2017 - 2019 Peter Robinson
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import model.TimeAwareEffectSize;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.monarchinitiative.phenotefx.gui.Platform;
 import org.monarchinitiative.phenotefx.gui.sigmoidchart.SigmoidChartFactory;
@@ -13,6 +34,7 @@ import org.monarchinitiative.phenotefx.service.Resources;
 
 import static org.junit.Assert.*;
 
+@Ignore
 public class RiskFactorFactoryTest extends Application {
 
     private Resources resources = new Resources(null, null, null, null);
