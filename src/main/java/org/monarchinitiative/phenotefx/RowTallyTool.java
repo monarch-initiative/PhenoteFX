7e3652dc626a71ab36871eb1ef3cf7d65fbc71a6
Peter Robinson <peter.robinson@jax.org>
2020-06-27 09:00:17 -0400

    adding tally tool for c&p rows

--- src/main/java/org/monarchinitiative/phenotefx/RowTallyTool.java
+++ src/main/java/org/monarchinitiative/phenotefx/RowTallyTool.java
@@ -1,5 +1,25 @@
 package org.monarchinitiative.phenotefx;
 
+/*-
+ * #%L
+ * PhenoteFX
+ * %%
+ * Copyright (C) 2017 - 2020 Peter Robinson
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
 import org.monarchinitiative.phenotefx.gui.infoviewer.InfoViewerFactory;
 
 import java.util.HashMap;
@@ -17,12 +37,23 @@ public class RowTallyTool {
 
     private final Map<String, Integer> items;
 
+    private final int totalUsableColumns;
+
     public RowTallyTool(String row) {
         items = new HashMap<>();
         String []fields = row.split("\t");
+        int N = 0;
         for (String f : fields) {
+            if (f.isEmpty()){
+                continue; // no observation
+            } else if (f.equalsIgnoreCase("n/a")) {
+                continue; // data not available
+            } else {
+                N++;
+            }
             String[] features = f.split("[,;.]");
             for (String feat : features) {
+                feat = feat.trim();
                 if (feat.isEmpty() || feat.equals("a") || feat.equals("n")) {
                     continue; // reduce the noise, somewhat
                 }
@@ -30,11 +61,13 @@ public class RowTallyTool {
                 items.merge(feat, 1, Integer::sum);
             }
         }
+        totalUsableColumns = N;
     }
 
     public void showTable() {
         StringBuilder sb = new StringBuilder();
         sb.append(getHTMLHead());
+        sb.append(getPara());
         sb.append(getTable());
         sb.append(getFooter());
         InfoViewerFactory.openDialog(sb.toString());
@@ -45,6 +78,13 @@ public class RowTallyTool {
         return  "<html><body>\n" +
                 inlineCSS() +
                 "<h1>PhenoteFX: Tallying Phenotypes from Clipboard (Row)</h1>";
+    }
+
+    private String getPara() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("<p>Entries tallied up from row of data about phenotypic abnormalities. We counted a ")
+                .append("total of ").append(totalUsableColumns).append(" columns with observed phenotype data.<p>");
+        return sb.toString();
     }
 
 
